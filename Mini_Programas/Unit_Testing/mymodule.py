09172efbde8568a3d0db0af2827b46a595f90d41
PabloGarzón <79424389+PabloGarzonB0@users.noreply.github.com>
2025-02-26 14:21:52 -0500

    Create mymodule.py

---
+++ Mini_Programas/Unit_Testing/mymodule.py
@@ -0,0 +1,20 @@
+"""
+Modulo para realizar multiples operaciones matematicas
+"""
+def square(number):
+    """
+    This function returns the square of a given number
+    """
+    return number ** 2
+
+def double(number):
+    """
+    This function return  twice the value of a given number
+    """
+    return number * 2
+
+def add(number1, number2):
+    '''Funcion para sumar 2 numeracos'''
+
+    return number1 + number2
+
