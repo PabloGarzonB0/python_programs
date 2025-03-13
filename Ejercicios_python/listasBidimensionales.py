22f33de15b989028432d38c3e38fb8b072fa426a
PabloGarzón <79424389+PabloGarzonB0@users.noreply.github.com>
2025-03-13 02:11:39 +0000

    Implementacion funcion de matriz de conteo numerico

--- Ejercicios_python/listasBidimensionales.py
+++ Ejercicios_python/listasBidimensionales.py
@@ -4,6 +4,10 @@ def generar_asientos(filas=3, columnas=6):
     """Generar una matriz de asientos con valores aleatorios de 0 y 1."""
     return [[random.randrange(0,2) for _ in range(columnas)] for _ in range(filas)]
 
+def contar_asientos_disponibles(asientos):
+    """cuenta los asientos disponibles en la matriz (representados por 0). """
+    return sum(fila.count(0) for fila in asientos)
+
 def imprimir_asientos(asientos):
     """Imprime la matriz de asientos y cuenta los asientos disponibles"""
     disponibles = 0
@@ -15,11 +19,25 @@ def imprimir_asientos(asientos):
         print()
     print(f"El numero de asietos dispoinbles en la sala es: {disponibles}")
 
+def crear_matriz_numerica(filas=4, columnas=4):
+    """Crear una matriz 4x4 con valores secuenciales del 1 al 16."""
+    return [[columnas * i + j + 1 for j in range(columnas)] for i in range(filas)]
+
+def imprimir_matriz(matriz):
+    """Imprime una matriz de forma estructurada """
+    for fila in matriz:
+        print(fila)
+
+
 def main():
     """Funcion principal que ejectua el programa."""
     # Sistema de reserva de asientos
     asientos = generar_asientos()
     imprimir_asientos(asientos)
+
+    # Creacion de matriz numerica
+    matriz_numerica = crear_matriz_numerica()
+    imprimir_matriz(matriz_numerica)
 
 
 if __name__ == "__main__":
