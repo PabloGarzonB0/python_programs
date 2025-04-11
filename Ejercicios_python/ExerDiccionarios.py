7b6d041a418b9e636a3005b3e7e0b6ad627648ee
PabloGarzón <79424389+PabloGarzonB0@users.noreply.github.com>
2025-04-11 00:15:13 -0500

    Update ExerDiccionarios.py

--- Ejercicios_python/ExerDiccionarios.py
+++ Ejercicios_python/ExerDiccionarios.py
@@ -210,6 +210,11 @@ if __name__ == "__main__":
     # Ejercicio 9
     gestionBaseDatos()
     # Ejercicio 10
-    datos_cliente = "nif;nombre;email;teléfono;descuento\n01234567L;Luis González;luisgonzalez@mail.com;656343576;12.5\n71476342J;Macarena Ramírez;macarena@mail.com;692839321;8\n63823376M;Juan José Martínez;juanjo@mail.com;664888233;5.2\n98376547F;Carmen Sánchez;carmen@mail.com;667677855;15.7"
+    datos_cliente = "nif;nombre;email;teléfono;descuento\n    \
+                    01234567L;Luis González;luisgonzalez@mail.com;656343576;12.5\n    \
+                    71476342J;Macarena Ramírez;macarena@mail.com;692839321;8\n        \
+                    63823376M;Juan José Martínez;juanjo@mail.com;664888233;5.2\n      \
+                    98376547F;Carmen Sánchez;carmen@mail.com;667677855;15.7"          
+    
     directorioClientes()
 
