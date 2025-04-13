292a2328bf11463df5ea8354f22d7d0a668ec5bf
PabloGarzón <79424389+PabloGarzonB0@users.noreply.github.com>
2025-04-12 23:57:30 -0500

    Update Functions_exer.py

--- Ejercicios_python/Functions_exer.py
+++ Ejercicios_python/Functions_exer.py
@@ -3,9 +3,19 @@ def greet():
   """ Function that show greetings on display """
   print(f'Hi Everyone, have a wonderful day')
   
+def factorialNumber(number):
+  if number == 0 or number == 1:
+     factorial = 1
+  else:
+    factorial = number * factorialNumber(number - 1 )
+  return factorial
+  
 
 
 # Punto de entrada principal del programa
 if __name__ = '__main__':
-  # Llamado a componentes
+  # Exercise 1
   greet()
+  # Exercise 2
+  number = 5  # We harcore a number that pass into the function
+  factorialNumber(number)
