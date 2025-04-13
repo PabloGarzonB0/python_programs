525d3c446fbe41618717ca95d6c1b85dd3ca5e37
PabloGarzón <79424389+PabloGarzonB0@users.noreply.github.com>
2025-04-12 23:47:04 -0500

    Create Functions_exer.py

---
+++ Ejercicios_python/Functions_exer.py
@@ -0,0 +1,11 @@
+# This script contain practical exercises usign functions in python
+def greet():
+  """ Function that show greetings on display """
+  print(f'Hi Everyone, have a wonderful day')
+  
+
+
+# Punto de entrada principal del programa
+if __name__ = '__main__':
+  # Llamado a componentes
+  greet()
