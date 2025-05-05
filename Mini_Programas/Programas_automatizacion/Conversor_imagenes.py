368f2c6de644e5de5143d4345c3c61b49a946633
PabloGarzón <79424389+PabloGarzonB0@users.noreply.github.com>
2025-05-04 23:58:52 -0500

    Update Conversor_imagenes.py

--- Mini_Programas/Programas_automatizacion/Conversor_imagenes.py
+++ Mini_Programas/Programas_automatizacion/Conversor_imagenes.py
@@ -2,3 +2,11 @@ import os
 from PIL import Image
 
 def listar_formatos_soportados():
+'''Muestra los formatos de imagen soportados '''
+  formatos = ["JPG", "JPEG", "PNG", "GIF", "BMP", "TIFF", "WEBP"]
+  print("Formatos soportados")
+  for formato in formatos:
+    print(f"- {formato}")
+  return formatos
+
+
