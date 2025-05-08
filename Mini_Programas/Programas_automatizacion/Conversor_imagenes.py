046932a7c2b716ae1e2aca6b573a5694652afb34
PabloGarzón <79424389+PabloGarzonB0@users.noreply.github.com>
2025-05-07 23:59:33 -0500

    Update Conversor_imagenes.py

--- Mini_Programas/Programas_automatizacion/Conversor_imagenes.py
+++ Mini_Programas/Programas_automatizacion/Conversor_imagenes.py
@@ -20,29 +20,37 @@ def convertir_imagen(ruta_imagen, formato_salida, carpeta_destino=None):
     str: Ruta de la imagen convertida
   """
   try:
-    "Verifica que al imagen existe"
-    if not os.path.exists(ruta_imagen):
-      print(f"Error: La imagen '{ruta_imagen}' no existe.")
-      return None
+      "Verifica que al imagen existe"
+      if not os.path.exists(ruta_imagen):
+        print(f"Error: La imagen '{ruta_imagen}' no existe.")
+        return None
+  
+      # Abrir la imagen
+      imagen = Image.open(ruta_imagen)
+      # Obtener informacion de la imagen original
+    nombre_archivo = os.path.basename(ruta_imagen)
+    nombre_base = os.path.splitext(nombre_archivo)[0]
+    # Determinar la carpeta de destino
+    if carpeta_destino is None:
+      carpeta_destino = os.path.urlname(ruta_imagen)
+    # Crear carpeta de destino si no existe
+    if not os.path.exists(carpeta_destino):
+      os.makedirs(carpeta_destino)
+  
+    # Crear ruta de salida
+    formato_salida = formato_salida.lower().strip(".")
+    ruta_salida = os.path.join(carpeta_destino, f"{nombre_base}.{formato_salida}")
+  
+    imagen.save(ruta_salida)
+    print(f"Imagen convertida y guardada en : {ruta_salida}")
+  
+    return ruta_salida
 
-    # Abrir la imagen
-    imagen = Image.open(ruta_imagen)
-    # Obtener informacion de la imagen original
-  nombre_archivo = os.path.basename(ruta_imagen)
-  nombre_base = os.path.splitext(nombre_archivo)[0]
-  # Determinar la carpeta de destino
-  if carpeta_destino is None:
-    carpeta_destino = os.path.urlname(ruta_imagen)
-  # Crear carpeta de destino si no existe
-  if not os.path.exists(carpeta_destino):
-    os.makedirs(carpeta_destino)
+except Exception as e:
+    print(f"Error al convertir la imagen: {e}")
+    return None
 
-  # Crear ruta de salida
-  formato_salida = formato_salida.lower().strip(".")
-  ruta_salida = os.path.join(carpeta_destino, f"{nombre_base}.{formato_salida}")
 
-  imagen.save(ruta_salida)
-  print(f"Imagen convertida y guardada en : {ruta_salida}")
 
 
 
